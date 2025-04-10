ef9705b1c9fea8a51e244dc3f3c6735b370a51c1
KingOfNinjas-Git <franciscomfcavaco@gmail.com>
2025-04-10 16:29:37 +0100

    Website Version 1.0

--- animation.js
+++ animation.js
@@ -87,3 +87,9 @@ function toggleMenu() {
 const nav = document.querySelector(".navigation");
 nav.classList.toggle("active");
  }
+
+ function togglePopup() {
+    const popup = document.getElementById('contact-popup');
+    popup.classList.toggle('hidden');
+  }
+
